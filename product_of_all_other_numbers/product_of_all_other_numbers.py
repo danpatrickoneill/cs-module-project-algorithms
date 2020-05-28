a7d9364b1d3d8a49fd953f7ee257c2f2c537e07a
danpatrickoneill <dan47573@gmail.com>
2020-05-28 14:20:28 -0400

    Solid solution w/ division; issues with zeroes

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -5,17 +5,23 @@ Returns: a List of integers
 def product_of_all_other_numbers(arr):
     # First pass
     # Create placeholder list for return
-    products = [1] * len(arr)
-    for i in range(len(products)):
-        for j in range(len(arr)):
-            if i == j:
-                continue
-            products[i] *= arr[j]
-    return products
+    # products = [1] * len(arr)
+    # for i in range(len(products)):
+    #     for j in range(len(arr)):
+    #         if i == j:
+    #             continue
+    #         products[i] *= arr[j]
+    # return products
+    # Refined solution; target is O(n) time with O(1) space
+    for i in range(1, len(arr)):
+        arr[0] *= arr[i]
+    for i in range(1, len(arr)):
+        arr[i] = arr[0] // arr[i]
+    return arr 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
