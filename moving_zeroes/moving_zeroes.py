93a565a94704edb17a73cb9d009822d1c795abdf
danpatrickoneill <dan47573@gmail.com>
2020-05-27 20:07:02 -0400

    Working solution in single pass with O(1) space

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -17,11 +17,13 @@ def moving_zeroes(arr):
     # Reflecting+refining; aim is single pass using O(1) space
     last_index = len(arr) - 1
     for i in range(len(arr)):
-        if last_index == i:
+        if last_index <= i:
             return arr
         if arr[i] == 0:
             while arr[last_index] == 0:
                 last_index -= 1
+                if last_index == i:
+                    return arr
             arr[i], arr[last_index] = arr[last_index], arr[i]
             last_index -= 1
         print(i, arr, last_index)
@@ -33,6 +35,6 @@ def moving_zeroes(arr):
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
-
+    arr = [0, 0, 0, 0, 0]
+    
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
